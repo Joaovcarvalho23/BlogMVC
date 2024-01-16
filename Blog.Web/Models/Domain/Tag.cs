a1581b18740282d6aa397c7f7924991f073b953e
Joao <joaovictorcordeiro00@gmail.com>
2024-01-16 19:52:02 -0300

    Criando a Model Tag em Domain

---
+++ Blog.Web/Models/Domain/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? ExibicaoNome { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; } //uma tag pode ter uma coleção de posts de blog
+    }
+}
