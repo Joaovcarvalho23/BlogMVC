d3542b0f47104fbdd3cdefcaaa42cfd490ee87d7
Joao <joaovictorcordeiro00@gmail.com>
2024-01-16 19:52:18 -0300

    Criando a Model BlogPost em Domain

---
+++ Blog.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿namespace Blog.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        public string? Cabecalho { get; set; }
+
+        public string? TituloPagina { get; set; }
+
+        public string? Conteudo { get; set; }
+
+        public string? DescricaoBreve { get; set; }
+
+        public string? URLDaImagem { get; set; }
+
+        public string? URLHandle { get; set; }
+
+        public DateTime DataPublicacao { get; set; }
+
+        public string? AutorDaPublicacao { get; set; }
+
+        public bool EhVisivel { get; set; }
+
+        public ICollection<Tag> Tags { get; set; } //um BlogPost pode ter várias tags (uma coleção de tags)
+    }
+}
