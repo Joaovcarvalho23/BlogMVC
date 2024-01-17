b7f3d41fabdc6c328c3934541d93eda760e3cd1e
Joao <joaovictorcordeiro00@gmail.com>
2024-01-17 17:49:28 -0300

    Fazendo a Migration para o banco de dados

---
+++ Blog.Web/Migrations/20240117202437_Migracao Inicial.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TabelaBlogPosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Cabecalho = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TituloPagina = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Conteudo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescricaoBreve = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    URLDaImagem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    URLHandle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataPublicacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AutorDaPublicacao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EhVisivel = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TabelaBlogPosts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TabelaTags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExibicaoNome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TabelaTags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlogPostTag",
+                columns: table => new
+                {
+                    BlogPostsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPostTag", x => new { x.BlogPostsId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_BlogPostTag_TabelaBlogPosts_BlogPostsId",
+                        column: x => x.BlogPostsId,
+                        principalTable: "TabelaBlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogPostTag_TabelaTags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "TabelaTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPostTag_TagsId",
+                table: "BlogPostTag",
+                column: "TagsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPostTag");
+
+            migrationBuilder.DropTable(
+                name: "TabelaBlogPosts");
+
+            migrationBuilder.DropTable(
+                name: "TabelaTags");
+        }
+    }
+}
