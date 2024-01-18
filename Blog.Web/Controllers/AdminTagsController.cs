7b8cbddf1629a94ddd523af401a8f03d88ea76e1
Joao <joaovictorcordeiro00@gmail.com>
2024-01-18 19:50:56 -0300

    Implementando a controller do Admin de adicionar uma nova Tag ao banco de dados

--- Blog.Web/Controllers/AdminTagsController.cs
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private BlogDbContext _blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            _blogDbContext = blogDbContext;
+        }
+
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapeando a Tag Request para a model domain Tag
+            var minhaTag = new Tag
+            {
+                Nome = addTagRequest.Nome,
+                ExibicaoNome = addTagRequest.ExibicaoNome
+            };
+
+            _blogDbContext.TabelaTags.Add(minhaTag);
+            _blogDbContext.SaveChanges();
+
+            return View("Add");
         }
     }
 }
